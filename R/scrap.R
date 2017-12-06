a968327b479d37d4f34ab07248c5a3d3d07faa7f
Bacou, Melanie (desktop) <mel@mbacou.com>
2017-12-06 06:44:21 -0500

    reviewed survey designs

--- R/scrap.R
+++ R/scrap.R
@@ -1,3 +1,5 @@
+## 2017.10.24 #######################################################################
+
 library(raster)
 
 xy = cbind(
@@ -26,4 +28,89 @@ plot(r,add=T,alpha=0.5)
 sampleRandom(mask(r, xy.sp),size = 10)
 xy.r <- extract(r, xy.sp)
 xy.r.sample <- lapply(xy.r, sample, 10)
+
+## 2017.12.05 #######################################################################
+
+svyCrossTab <- function(f, by, design, quantile="Q50") {
+
+  # Pass a list of formulas (list(~var1, ~var2, ...))
+  # Then use tables::tabular() to format pivot tables
+
+  require(survey)
+  require(data.table)
+  require(stringr)
+
+  # Reshape means
+  m <- lapply(f, function(x) try(
+    svyby(x, by, design, svymean, na.rm=TRUE) %>%
+      ftable() %>% as.data.table())
+  )
+  m <- m[sapply(m, function(x) class(x)[1])!="try-error"]
+  m <- lapply(m, function(x) {
+    setnames(x, names(x)[names(x) %like% "Var"], c("Stat", "Variable"))
+    x[Stat=="svymean", Stat := "Mean"]
+  })
+  res <- rbindlist(m)
+
+  # Reshape quantiles
+  if(!is.na(quantile)) {
+    m <- lapply(f, function(x) try(
+      svyby(x, by, design, svyquantile, quantiles, ci=TRUE, na.rm=TRUE) %>%
+        ftable %>% as.data.table())
+    )
+    m <- m[sapply(m, function(x) class(x)[1])!="try-error"]
+    m <- lapply(m, function(x) {
+      n <- ncol(x)
+      setnames(x, names(x)[names(x) %like% "Var"], c("Stat", "Variable"))
+      x[Stat=="svyquantile", Stat := "Q50"]
+      x[Stat=="SE", Stat := "SEQ50"]
+    })
+    m <- rbindlist(m)
+    res <- rbind(res, m)
+  }
+
+  # Add variable labels
+  res <- dcast(res, ...~Stat, value.var="Freq")
+  res[, Variable := str_replace(Variable, fixed("I(1000 * "), "") %>%
+      str_replace(fixed("I(100 * "), "") %>% str_replace(fixed(")"), "")]
+  setkey(res, Variable)
+  setkey(lbl, varCode)
+  res[lbl, Label := i.varLabel]
+  return(res)
+}
+
+svyCrossProp <- function(x, design) {
+  if(length(x)==1) f <- formula(paste0("~", x))
+  if(length(x) > 1) f <- formula(paste0("~I(interaction(", paste(x, collapse=", "), "))"))
+  res <- svymean(f, design, na.rm=TRUE)
+  rn <- row.names(ftable(res)) %>%
+    str_replace(fixed(as.character(f)[2]), "") %>% str_split(fixed(".")) %>%
+    as.data.table() %>% t()
+  res.cv <- confint(res)
+  res <- as.data.table(cbind(res*100, res.cv*100))
+  setnames(res, c("mean", "CI1", "CI2"))
+  res[CI1<0, CI1 := 0]
+  res <- cbind(rn, res)
+  res[CI1 < CI1, CI1 := 0]
+  res[, `:=`(
+    mean = round(mean, digits=2),
+    CI1 = round(CI1, digits=1),
+    CI2 = round(CI2, digits=1)
+  )]
+  res[, `:=`(
+    CI = paste(CI1, CI2, sep=" - "),
+    CI1 = NULL,
+    CI2 = NULL
+  )]
+  setnames(res, seq_along(x), x)
+  for (i in x) res[[i]] <- factor(res[[i]], levels=levels(gha[[i]]))
+  return(res)
+}
+
+
+# Export by wave, class5, rural, quadrants, farm sizes, and produce marginal stats
+tmp <- svyCrossProp(c("croparea_3clas", "rural", "class5", "seg_quad"), eth.svy.farm[["eth2"]])
+tmp <- svyCrossTab(list(~croparea_imp, ~rural, ~class5), by=~croparea_4ha, eth.svy.farm[["eth2"]])
+tmp <- svyCrossTab(list(~croparea_imp), by=~croparea_4ha, eth.svy.farm[["eth2"]])
+
 
